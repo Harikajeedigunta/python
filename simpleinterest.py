dc2a2a8fd4d93a951b8f080b3d3aa1016369a053
Harika_Jeedigunta <84392557+Harikajeedigunta@users.noreply.github.com>
2023-07-22 19:55:58 +0530

    Create simpleinterest.py

---
+++ simpleinterest.py
@@ -0,0 +1,16 @@
+p,t,r=map(int,input().split())
+SI=(p*t*r)/100
+print(SI)
+
+def simple_interest(p,t,r):
+    print('The principal is', p)
+    print('The time period is', t)
+    print('The rate of interest is',r)
+     
+    si = (p * t * r)/100
+     
+    print('The Simple Interest is', si)
+    return si
+     
+# Driver code
+simple_interest(8, 6, 8)
