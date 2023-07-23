653536424dc593dd1a0018ae87ef6b29d5c52903
Harika_Jeedigunta <84392557+Harikajeedigunta@users.noreply.github.com>
2023-07-23 18:52:43 +0530

    Create circlearea.py

---
+++ circlearea.py
@@ -0,0 +1,13 @@
+import math
+def area(r):
+    area=math.pi*pow(r,2)
+    print('Area of circle is:' ,area)
+area(r)
+
+def area(r):
+    PI=3.14
+    area=PI*(r*r)
+    print("area is %.6f" %area)
+r=int(input())
+area(r)
+
